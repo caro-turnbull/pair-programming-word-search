a347ca45c790110b544cff329db569fe8ba43ca0
caro-turnbull <caro.turnbull@gmail.com>
2023-06-07 14:34:38 -0400

    finished

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,66 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+  
+  const horizontalJoin = letters.map(array => array.join(''))
+  for (lines of horizontalJoin) {
+      if (lines.includes(word)) return true
+  }
+
+  const transposed = transpose(letters)
+  const transposeHorizJoin = transposed.map(array => array.join(''))
+  for (lines of transposeHorizJoin) {
+      if (lines.includes(word)) return true
+    
+  }
+  return false;
 }
 
 module.exports = wordSearch
+
+const transpose = function(letters) {
+  const transposed = [];
+  for (let row = 0; row < letters[0].length; row++) {
+    const newArr = [];
+    for (let col = 0; col < letters.length; col ++) {
+      newArr.push(letters[col][row]);
+    }
+    transposed.push(newArr);
+  }
+
+  return transposed;
+};
+
+
+
+
+// // Do not edit this function.
+// const printMatrix = (matrix) => {
+//   for (const row of matrix) {
+//     for (const el of row) {
+//       process.stdout.write(el + " ");
+//     }
+//     process.stdout.write("\n");
+//   }
+// };
+
+// printMatrix(
+//   transpose([
+//     [1, 2, 3, 4],
+//     [1, 2, 3, 4],
+//     [1, 2, 3, 4],
+//     [1, 2, 3, 4],
+//   ])
+// );
+
+// console.log("----");
+
+// printMatrix(
+//   transpose([
+//     [1, 2],
+//     [3, 4],
+//     [5, 6],
+//   ])
+// );
+
+// console.log("----");
+
+// printMatrix(transpose([[1, 2, 3, 4, 5, 6, 7]]))
